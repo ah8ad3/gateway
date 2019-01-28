0c50e24f043c30e65a89aa654613e6daead274cc
ahmadzoli <ah8ad3@gmail.com>
2019-01-28 19:56:38 +0330

    service structure created, working on routes regex for services

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # gateway
 simple api gateway with golang
 
+# Features
+- define all services in services.json file
+
+# TODO
+- working on routes regex
+
 I dont implement api gateway with all standard features because i dont need them now.
 i will implement a simple api gateway for my private project, after that i will implement more good features
--- main.go
+++ main.go
@@ -2,15 +2,14 @@ package main
 
 import (
 	"fmt"
-	"github.com/go-chi/chi"
+	"github.com/ah8ad3/gateway/routes"
 	"net/http"
 )
 
 func main() {
-	r := chi.NewRouter()
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		_, _ = w.Write([]byte("welcome"))
-	})
+	routes.LoadServices()
+	routes.CheckServices()
+	r := routes.Routes()
 	fmt.Println("Server run at :3000")
 	_ = http.ListenAndServe(":3000", r)
 }
