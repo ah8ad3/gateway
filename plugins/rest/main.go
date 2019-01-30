85ea13722dd55af25d16565b0dab7cd4fa79534d
ahmadzoli <ah8ad3@gmail.com>
2019-01-30 23:26:10 +0330

    get service added

--- plugins/rest/main.go
+++ plugins/rest/main.go
@@ -1,1 +1,12 @@
 package rest
+
+import (
+	"encoding/json"
+	"github.com/ah8ad3/gateway/pkg/routes"
+	"net/http"
+)
+
+func GetServices(w http.ResponseWriter, r *http.Request) {
+	data, _ := json.Marshal(routes.Service)
+	_, _ = w.Write(data)
+}
