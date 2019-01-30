96f54aa88db5b2b15536ee296ee292e0da90ea61
ahmadzoli <ah8ad3@gmail.com>
2019-01-30 18:10:18 +0330

    mongo for auth configed, sign jwt added

--- pkg/auth/service.go
+++ pkg/auth/service.go
@@ -1,9 +1,21 @@
 package auth
 
 import (
+	"context"
+	"fmt"
+	"github.com/ah8ad3/gateway/pkg/logger"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/mongodb/mongo-go-driver/bson"
+	"github.com/mongodb/mongo-go-driver/mongo"
 	"golang.org/x/crypto/bcrypt"
 	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
 )
+
+var collection *mongo.Collection
 
 func hashAndSalt(pwd []byte) string {
 
@@ -37,51 +49,102 @@ func comparePasswords(hashedPwd string, plainPwd []byte) bool {
 	return true
 }
 
+func OpenAuthCollection() {
+	collection = logger.DB.Collection("auth")
+}
+
 
 // CheckJwt if token is JWT and have time
-//func CheckJwt(w http.ResponseWriter, r *http.Request) {
-//	_ = r.ParseForm()
-//	tokenstring := strings.Join(r.Form["token"], "")
-//	token, _ := jwt.Parse(tokenstring, func(token *jwt.Token) (interface{}, error) {
-//		return []byte(os.Getenv("SECRET_KEY")), nil
-//	})
-//	// When using `Parse`, the result `Claims` would be a map.
-//
-//	// In another way, you can decode token to your struct, which needs to satisfy `jwt.StandardClaims`
-//	user := JWT{}
-//	token, _ = jwt.ParseWithClaims(tokenstring, &user, func(token *jwt.Token) (interface{}, error) {
-//		return []byte(os.Getenv("SECRET_KEY")), nil
-//	})
-//	if token.Valid {
-//		_, _ = fmt.Fprintf(w, "this token is right")
-//		return
-//	}
-//	_, _ = fmt.Fprintf(w, "this token is wrong")
-//	return
-//}
+func CheckJwt(w http.ResponseWriter, r *http.Request) {
+	_ = r.ParseForm()
+	tokenstring := strings.Join(r.Form["token"], "")
+	token, _ := jwt.Parse(tokenstring, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("SECRET_KEY")), nil
+	})
+	// When using `Parse`, the result `Claims` would be a map.
+
+	// In another way, you can decode token to your struct, which needs to satisfy `jwt.StandardClaims`
+	user := JWT{}
+	token, _ = jwt.ParseWithClaims(tokenstring, &user, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("SECRET_KEY")), nil
+	})
+	if token.Valid {
+		_, _ = fmt.Fprintf(w, "this token is right")
+		return
+	}
+	_, _ = fmt.Fprintf(w, "this token is wrong")
+	return
+}
 
 // SignJWT function to create jwt token
-//func SignJWT(w http.ResponseWriter, r *http.Request) {
-//	if r.Method == "POST" {
-//		_ = r.ParseForm()
-//		username := strings.Join(r.Form["username"], "")
-//		password := strings.Join(r.Form["password"], "")
-//
-//		if username == "" || password == "" {
-//			_, _ = fmt.Fprintf(w, "input form are incomplete")
-//			return
-//		}
-//
-//		user := User{}
-//		DB.Where("username = ?", username).First(&user)
-//
-//		if user.ID == 0 || !ValidPassword(user.Password, password){
-//			_, _ = fmt.Fprintf(w, "username or password wrong")
-//			return
-//		}
-//		token := createTokenString(user)
-//		_, _ = fmt.Fprintf(w, token)
-//		return
-//	}
-//}
+func SignJWT(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		_ = r.ParseForm()
+		username := strings.Join(r.Form["username"], "")
+		password := strings.Join(r.Form["password"], "")
+
+		if username == "" || password == "" {
+			_, _ = fmt.Fprintf(w, "input form are incomplete")
+			return
+		}
+
+		pointer := collection.FindOne(context.Background(), bson.D{{"username", username}})
+		raw, err := pointer.DecodeBytes()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if raw == nil {
+			_, _ = fmt.Fprintf(w, "username or password wrong")
+			return
+		}
+
+		_username := string(raw.Lookup("username").Value[:])
+		_password := string(raw.Lookup("password").Value[:])
+
+		if username != _username || !ValidPassword(_password, password) {
+			_, _ = fmt.Fprintf(w, "username or password wrong")
+			return
+		}
+
+		token := createTokenString(User{Username: username, Password: password})
+		_, _ = fmt.Fprintf(w, token)
+		return
+	}
+}
+
+func createTokenString(user User) string {
+	expireToken := time.Now().Add(time.Hour * 24).Unix()
+	// Embed User information to `token`
+	token := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), &JWT{
+		User: user,
+		Age:  30,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expireToken,
+		},
+	})
+
+	// token -> string. Only server knows this secret (foobar).
+	tokenstring, err := token.SignedString([]byte(os.Getenv("SECRET_KEY")))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return tokenstring
+}
+
+
+
+func ShowLogs() {
+	// bson.D{{}} return all records in database you can create a map here
+	//logg := logger.UserLog{}
+	data1, _ := logger.Collection.Find(context.Background(), bson.D{{"log.event", "critical"}})
+
+	for data1.Next(context.Background()) {
+		raw, err := data1.DecodeBytes()
+		if err != nil { log.Fatal(err) }
+		logg := logger.Log{}
+		_ = raw.Lookup("log").Unmarshal(&logg)
+		fmt.Println(logg)
+	}
+}
 
