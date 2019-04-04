3749fe2c69d52460445040545b40f83be912e51b
Ahmad <ah8ad3@gmail.com>
2019-04-04 13:50:26 +0430

    DB directory tests (#6)
    * working on develop branch
    
    * bug in test path db fixed
    
    * more tests in db path
    
    * coverage goes to 77

---
+++ pkg/db/methods_test.go
@@ -0,0 +1,56 @@
+package db
+
+import (
+	"encoding/json"
+	"os"
+	"testing"
+)
+
+type sample struct {
+	name string
+}
+
+var samples []sample
+
+func TestBefore(t *testing.T) {
+	samples = append(samples, sample{name: "test"})
+	samples = append(samples, sample{name: "test1"})
+	samples = append(samples, sample{name: "test2"})
+}
+
+func TestGenerateSecretKey(t *testing.T) {
+	if err := os.RemoveAll("./../../db/"); err != nil {
+		t.Fatal(err.Error())
+	}
+	LoadSecretKey()
+	GenerateSecretKey()
+
+	LoadSecretKey()
+	GenerateSecretKey()
+
+	if nl := GetProxies(); nl != nil {
+		t.Fatal("nl must be nil")
+	}
+}
+
+func TestInsertProxy(t *testing.T) {
+	jData, _ := json.Marshal(samples)
+	InsertProxy(jData)
+	InsertProxy(jData)
+
+}
+
+func TestGetProxies(t *testing.T) {
+	GetProxies()
+}
+
+func TestInsertPlugins(t *testing.T) {
+	jData, _ := json.Marshal(samples)
+	InsertPlugins(jData)
+	InsertPlugins(jData)
+
+}
+
+func TestGetPlugins(t *testing.T) {
+	GetPlugins()
+}
