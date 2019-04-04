b321e603bd4a1a405fa63daf7c94a32528caaa9b
ah8ad3 <ah8ad3@gmail.com>
2019-04-04 12:45:14 +0430

    more tests in db path

--- pkg/db/methods_test.go
+++ pkg/db/methods_test.go
@@ -1,22 +1,49 @@
 package db
 
 import (
+	"encoding/json"
 	"os"
 	"testing"
 )
 
+type sample struct {
+	name string
+}
+
+var samples []sample
+
+func TestBefore(t *testing.T) {
+	samples = append(samples, sample{name: "test"})
+	samples = append(samples, sample{name: "test1"})
+	samples = append(samples, sample{name: "test2"})
+}
+
 func TestGenerateSecretKey(t *testing.T) {
-	LoadSecretKey()
-
-	GenerateSecretKey()
-
-	if err := os.RemoveAll("./../../db/secret.bin"); err != nil {
+	if err := os.RemoveAll("./../../db/"); err != nil {
 		t.Fatal(err.Error())
 	}
 	LoadSecretKey()
 	GenerateSecretKey()
+
+	LoadSecretKey()
+	GenerateSecretKey()
+
+	if nl := GetProxies(); nl != nil {
+		t.Fatal("nl must be nil")
+	}
+}
+
+func TestInsertProxy(t *testing.T) {
+	jData, _ := json.Marshal(samples)
+	InsertProxy(jData)
+	InsertProxy(jData)
+
+	SecretKey = ""
+	InsertProxy(jData)
 }
 
 func TestGetProxies(t *testing.T) {
-	//GetProxies()
+	GetProxies()
+	LoadSecretKey()
+	GetProxies()
 }
